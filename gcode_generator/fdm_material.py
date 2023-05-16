47bc18364534fb5928f9f32e5d5c7d40922fbfed
Frederic Meeuwissen <13856291+Frederic98@users.noreply.github.com>
2023-05-16 08:36:32 +0200

    Fix GIT material filename

--- gcode_generator/fdm_material.py
+++ gcode_generator/fdm_material.py
@@ -16,13 +16,18 @@ class FDMReader:
     Class to read fdm_material files.
      Can read local files, or download a profile from GitHub using 'git:generic_pla[@commit_id]'
     """
+    EXTENSION = '.xml.fdm_material'
+
     def __init__(self, filename: str):
         if filename.startswith('git:'):
             filename = filename.removeprefix('git:')
-            self.data = self.download_material(filename)
+            commit = 'master'
+            if '@' in filename:
+                filename, commit = filename.split('@', 1)
+            filename = self.assert_suffix(filename, self.EXTENSION)
+            self.data = self.download_material(filename, commit)
         else:
-            if not filename.endswith('.xml.fdm_material'):
-                filename = filename + '.xml.fdm_material'
+            filename = self.assert_suffix(filename, self.EXTENSION)
             with open(filename, 'rb') as file:
                 self.data = file.read()
         self.filename = filename
@@ -38,12 +43,14 @@ class FDMReader:
         return self.data
 
     @staticmethod
-    def download_material(filename):
-        if '@' in filename:
-            filename, commit = filename.split('@', 1)
-        else:
-            commit = 'master'
-        filename = filename + '.xml.fdm_material'
+    def assert_suffix(string: str, suffix: str):
+        if not string.endswith(suffix):
+            string = string + suffix
+        return string
+
+    @classmethod
+    def download_material(cls, filename, commit='master'):
+        filename = cls.assert_suffix(filename, cls.EXTENSION)
         url = f'https://raw.githubusercontent.com/Ultimaker/fdm_materials/{commit}/{filename}'
         with urllib.request.urlopen(url) as file:
             return file.read()
